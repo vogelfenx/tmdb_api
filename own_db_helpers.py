c2e55307bd724e5c52ba166acf4d11d2f10234e3
Kirill Fedyakov <vogelfenix@gmail.com>
2021-12-03 12:34:10 +0100

    Restructure project

--- own_db_helpers.py
+++
@@ -1,9 +0,0 @@
-import json
-import os
-
-def load_data(path):
-    if not os.path.exists(path):
-        return None
-    with open(path, mode='r', encoding='utf-8') as my_file:
-        films_data = json.load(my_file)
-        return films_data
