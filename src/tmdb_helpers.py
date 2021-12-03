23dc3be6e90b2e59fb4ce0ed368cf0f181f7851c
Kirill Fedyakov <vogelfenix@gmail.com>
2021-12-03 12:52:17 +0100

    Refactor code

--- src/tmdb_helpers.py
+++ src/tmdb_helpers.py
@@ -3,13 +3,15 @@ import urllib.parse
 import json
 from getpass import getpass
 
+from configuration import settings
+
 
 def make_tmdb_api_request(method, api_key, extra_params=None):
     extra_params = extra_params or {}
     url = 'https://api.themoviedb.org/3%s' % method
     params = {
         'api_key': api_key,
-        'language': 'ru',
+        'language': 'en',
     }
     params.update(extra_params)
     return load_json_data_from_url(url, params)
@@ -22,7 +24,10 @@ def load_json_data_from_url(base_url, url_params):
 
 
 def get_user_api_key():
-    user_api_key = getpass('Enter your api key v3:')
+    user_api_key = getpass(('Enter your api key v3 '
+                            'or keep it empty (env will be checked):'))
+    if not user_api_key:
+        user_api_key = settings.tmdb_api_key_v3
     try:
         make_tmdb_api_request(method='/movie/2', api_key=user_api_key)
         return user_api_key
